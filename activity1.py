3d52ecb47b12e7b950aee8a52fbc98b7bad29ea3
Aliza <59551300+sabeeka131@users.noreply.github.com>
2024-12-20 18:43:01 +0500

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,9 @@
+#input an integer value
+n = int(input("Enter the number whose sum you want to find: "))
+sum=0
+
+
+#Interates for n+1 times: i=1 to n+1
+for i in range(1, n+1):
+    sum = sum+i
+    print("\nSum =", sum)
